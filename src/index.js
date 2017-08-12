73c490f981e7bb56ac8b568da393b6370c8d1932
Pontus Persson <pontus.alexander@gmail.com>
2017-08-12 15:00:16 +0200

    Allow timeout to be set.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-function desensitizeMouseMove(callback, sluggishness = 5) {
+function desensitizeMouseMove(callback, sluggishness = 5, timeout = 500) {
   let next = 0;
   let count = 0;
   return function handleMouseMove(event) {
     const now = Date.now();
     if (next < now) {
       count = 0;
-      next = now + 500;
+      next = now + timeout;
     }
     if (++count > sluggishness) {
       callback(event);
